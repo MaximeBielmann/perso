76c95eab210afdf45ffb3d63e94d75f1928ee0e8
MaximeBielmann <bielmann.maxime@gmail.com>
2019-10-28 14:18:41 +0000

    try to add all js files to assets.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,4 +10,4 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-Rails.application.config.assets.precompile += %w( main.js )
+Rails.application.config.assets.precompile += %w( main.js animated-headline.js app.js application.js bootstrap.min.js cable.js html5shiv.min.js isotope.pkgd.min.js jquery.counterup.min.js jquery.waypoints.min.js lightbox.js lity.min.js map-white.js owl.carousel.min.js particles.js popper.min.js respond.min.js scrollIt.min.js validator.js wow.min.js)
