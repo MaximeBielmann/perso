59e91287da1fa2561e021e5dda30b770091b93d3
MaximeBielmann <bielmann.maxime@gmail.com>
2019-10-29 21:06:05 +0000

    add delete portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -5,14 +5,14 @@ class PortfoliosController < ApplicationController
     end
     
     def show
-         @portfolio = Portfolio.find(params[:id])
-    end
-    
-    def update
         if session[:user_id]
           @current_user = User.find(session[:user_id])
         end
         
+         @portfolio = Portfolio.find(params[:id])
+    end
+    
+    def update
         @portfolio = Portfolio.find(params[:id])
         @portfolio.update portfolio_cat: params[:portfolio_cat], portfolio_image_link: params[:portfolio_image_link], portfolio_image_alt: params[:portfolio_image_alt], portfolio_title: params[:portfolio_title], portfolio_desc: params[:portfolio_desc]
         redirect_to "/portfolios/#{params[:id]}"
