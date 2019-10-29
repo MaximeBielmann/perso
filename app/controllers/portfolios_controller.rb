bdf0eaec00bcb1936cd619e975b1add42deb9264
MaximeBielmann <bielmann.maxime@gmail.com>
2019-10-29 20:53:43 +0000

    fix update all

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -9,8 +9,13 @@ class PortfoliosController < ApplicationController
     end
     
     def update
-        Portfolio.update portfolio_cat: params[:portfolio_cat], portfolio_image_link: params[:portfolio_image_link], portfolio_image_alt: params[:portfolio_image_alt], portfolio_title: params[:portfolio_title], portfolio_desc: params[:portfolio_desc]
-        redirect_to "/admin/"
+        if session[:user_id]
+          @current_user = User.find(session[:user_id])
+        end
+        
+        @portfolio = Portfolio.find(params[:id])
+        @portfolio.update portfolio_cat: params[:portfolio_cat], portfolio_image_link: params[:portfolio_image_link], portfolio_image_alt: params[:portfolio_image_alt], portfolio_title: params[:portfolio_title], portfolio_desc: params[:portfolio_desc]
+        redirect_to "/portfolios/#{params[:id]}"
     end
     
     def destroy
