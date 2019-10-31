1aba0e271ed2ee888a2521a009e2448a82110ba1
MaximeBielmann <bielmann.maxime@gmail.com>
2019-10-31 15:46:47 +0000

    add list of projets in /admin

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,5 +7,7 @@ class PagesController < ApplicationController
     if session[:user_id]
       @current_user = User.find(session[:user_id])
     end
+    
+    @portfolios = Portfolio.all
   end
 end
