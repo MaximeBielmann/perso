458ec4c21188f451317b75b28639cb8cfa4f280f
MaximeBielmann <bielmann.maxime@gmail.com>
2019-11-02 10:33:42 +0000

    try to add meta tags gem

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,9 @@
 class PagesController < ApplicationController
   def home
+    @page_title = "Accueil"
+    @page_description = "Je vous accompagne de l'idée jusqu'à la réalisation de votre projet, du cahier des charges à l'utilisation quotidienne de"
+    @page_keywords = "développeur web"
+    
     @portfolios = Portfolio.all
   end
   
