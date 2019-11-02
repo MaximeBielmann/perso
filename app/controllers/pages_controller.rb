9557bf1ce5027893d6974bc3ecab797e688281f7
MaximeBielmann <bielmann.maxime@gmail.com>
2019-11-02 10:44:45 +0000

    try to fix meta tags

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def home
-    @page_title = "Accueil"
+    @page_title = 'Accueil'
     @page_description = "Je vous accompagne de l'idée jusqu'à la réalisation de votre projet, du cahier des charges à l'utilisation quotidienne de"
     @page_keywords = "développeur web"
     
